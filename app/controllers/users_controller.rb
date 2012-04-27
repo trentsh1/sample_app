e8cde7cfc7f324b179b982b6c6e9050f203b9c5b
Trent Sheffer <trentsh1@umbc.edu>
2012-04-26 21:36:53 -0500

    Added user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
 
 	def show
 		@user = User.find(params[:id])
+		@microposts = @user.microposts.paginate(:page => params[:page])
 		@title = @user.name
 	end
 
@@ -64,10 +65,6 @@ class UsersController < ApplicationController
 	
 	private
 
-		def authenticate
-			deny_access unless signed_in?
-		end
-
 		def correct_user
 			@user = User.find(params[:id])
 			redirect_to(root_path) unless current_user?(@user)
@@ -77,4 +74,5 @@ class UsersController < ApplicationController
 			@user = User.find(params[:id])
 			redirect_to(root_path) if !current_user.admin? || current_user?(@user)
 		end
+
 end
